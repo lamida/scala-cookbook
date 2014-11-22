74b5068b5966fe9df0fc59475829c6717eaad4df
Jon Kartago Lamida <me@lamida.net>
2014-11-22 22:22:29 +0800

    add 13_6

---
+++ src/main/scala/13_6.scala
@@ -0,0 +1,19 @@
+package actortests
+
+import akka.actor._
+
+class TestActor extends Actor {
+  def receive = {
+    case _ => println("a message was received")
+  }
+}
+
+object SystemStopExample extends App {
+  val actorSystem = ActorSystem("SystemStopExample")
+  val actor = actorSystem.actorOf(Props[TestActor], name = "test")
+  actor ! "hello"
+
+  actorSystem.stop(actor)
+  actorSystem.shutdown
+}
+
